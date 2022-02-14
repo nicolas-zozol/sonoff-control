1c6974cf852fa3a051d938e34a7f7af75f510891
Nicolas Zozol <nzozol@gmail.com>
2022-02-14 20:47:50 +0100

    Adding true oracle api

--- src/oracle/data.js
+++ src/oracle/data.js
@@ -1,11 +1,36 @@
+const axios = require("axios")
+const {getLogger} = require('../../logger/logger-factory');
+const logger = getLogger("APP");
+
+let values = []
+
 module.exports = function(){
-  return {
-    eth:2800,
-    weth:2800,
-    dai:1,
-    usdc:1,
-    usdt:1,
-    btc:38800,
-    wbtc:38800,
+  return values
+}
+
+loadData()
+setInterval(loadData, 5*60*1000)
+
+async function loadData(){
+  try {
+    const response = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=50&convert=USD', {
+      headers: {
+        'X-CMC_PRO_API_KEY': process.env.CMC_KEY
+      },
+    });
+    const json = response.data;
+    values = json.data.map(t=>({price:t.quote.USD.price, symbol:t.symbol}))
+    values.push({
+      price:values.find(t=>t.symbol === 'BTC').price,
+      symbol:'WBTC'}
+    )
+    values.push({
+      price:values.find(t=>t.symbol === 'ETH').price,
+      symbol:'WETH'}
+    )
+    console.log('**** updated data', values[0], values[1])
+
+  } catch(ex) {
+    logger.error(JSON.stringify(ex))
   }
 }
